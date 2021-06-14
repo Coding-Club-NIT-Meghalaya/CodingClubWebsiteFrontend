7ea98f57649007bfeb533072ae5f061e4ff8d4a5
akhileshkumar234 <akhilesh13062001@gmail.com>
2021-06-14 16:08:44 +0530

    programming page updated

--- docs/Event Page/Scripts/script.js
+++ docs/Event Page/Scripts/script.js
@@ -97,7 +97,28 @@ async function getImage(){
         myHtml += '<a class=" carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>'
 
         carousel.innerHTML += myHtml;
+        
+    }
+}
 
+get_programming_data()
+
+async function get_programming_data(){
+    const response = await fetch('https://codingclubnitm.herokuapp.com/api/v1/event/');
+    const data = await response.json();
+
+    const caro1 = document.getElementById("culture1");
+    const url = 'https://codingclubnitm.herokuapp.com/api/v1/programming/';
+    for(let i=1;i<data.count; i++)
+    {
+        let myHtml = '<div><div class="card overflow-hidden flex-wrap"><img src="'+url + data.programming_data[i].filename +'" class="card-img-top">';
+        myHtml += '<div class="card-body" style="margin-top:0px;"><h4 class="title">'+data.programming_data[i].Title + '</h4>';
+        myHtml += '<div class="d-flex flex-column"><span class="date">Start Date:'+data.programming_data[i].StartDate.subtring(0,10);
+        myHtml += '</span><span class="date">End Date:'+data.programming_data[i].EndDate.subtring(0,10)+'</span>';
+        myHtml += '<span class="date">Time:'+data.programming_data[i].Time +'</span></div>';
+        myHtml += '<p class="card-text">'+ data.programming_data[i].ShortDescription.substring(0,60) +'... <a href="'+data.programming_data[i].Link +'">Go To event</a></p>';
+        myHtml += ' <a href="'+data.programming_data[i].RegistrationLink +'" <button type="button" class="button">Register Now</button></a></div></div>';
+        caro1.innerHTML+=myHtml;
     }
 }
 
