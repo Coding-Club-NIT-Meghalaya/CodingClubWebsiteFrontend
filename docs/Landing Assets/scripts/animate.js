2c33b800dfc289d7a0bede00ba448350d058d134
Utkarsh Kumar <utkarshkanswal@gmail.com>
2021-07-05 10:56:21 +0530

    Fixed pop up

--- docs/Landing Assets/scripts/animate.js
+++ docs/Landing Assets/scripts/animate.js
@@ -57,24 +57,6 @@
 
 ////////////// popup ////////////////////////////////////////
 
-$(function () {
-
-  // contact form animations
-  $('#contact').click(function () {
-    $('#contactForm').fadeToggle();
-  })
-  $(document).mouseup(function (e) {
-    var container = $("#contactForm");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-      &&
-      container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-      container.fadeOut();
-    }
-  });
-
-});
 
 
 
---
+++ docs/Landing Assets/scripts/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#contact').click(function() {
+        $('#contactform').fadeToggle();
+    })
+    $(document).mouseup(function(e) {
+        var container = $("#contactform");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            &&
+            container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.fadeOut();
+        }
+    });
+});
